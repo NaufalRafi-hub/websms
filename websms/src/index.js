6597bea9b9ea18fedaf4541919c66a1f74f3d283
NaufalRafi-hub <rafi.harahaps@gmail.com>
2021-06-01 16:33:36 +0700

    adding fullpage.js

--- websms/src/index.js
+++ websms/src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
+import ReactFullpage from "@fullpage/react-fullpage";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
+
+class FullpageWrapper extends React.Component {
+  onLeave(origin, destination, direction) {
+    console.log("Leaving section " + origin.index);
+  }
+  afterLoad(origin, destination, direction) {
+    console.log("After load: " + destination.index);
+  }
+  render() {
+    return (
+      <ReactFullpage
+        scrollOverflow={true}
+        sectionsColor={["white"]}
+        onLeave={this.onLeave.bind(this)}
+        afterLoad={this.afterLoad.bind(this)}
+        render={({ state, fullpageApi }) => {
+          return (
+            <div id="fullpage-wrapper">
+              <div className="section section1">
+                <h3>Section 1</h3>
+              </div>
+              <div className="section">
+                <div className="slide">
+                  <h3>Slide 2.1</h3>
+                </div>
+                <div className="slide">
+                  <h3>Slide 2.2</h3>
+                </div>
+                <div className="slide">
+                  <h3>Slide 2.3</h3>
+                </div>
+              </div>
+              <div className="section">
+                <h3>Section 3</h3>
+                <button onClick={() => fullpageApi.moveTo(1, 0)}>
+                  Move top
+                </button>
+              </div>
+            </div>
+          );
+        }}
+      />
+    );
+  }
+}
+
+ReactDOM.render(<FullpageWrapper />, document.getElementById("react-root"));
+
+export default FullpageWrapper;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
