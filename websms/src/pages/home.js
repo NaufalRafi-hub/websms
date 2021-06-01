fa8e4e46031abfee1b91901eca734369361ea72b
NaufalRafi-hub <rafi.harahaps@gmail.com>
2021-06-01 22:06:51 +0700

    updated structure

---
+++ websms/src/pages/home.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
+import ReactFullpage from "@fullpage/react-fullpage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles-home.css";
+import Navbar from "../Components/Navbar/Navbar";
+
+class FullpageWrapper extends React.Component {
+  onLeave(origin, destination, direction) {
+    console.log("Leaving section " + origin.index);
+  }
+  afterLoad(origin, destination, direction) {
+    console.log("After load: " + destination.index);
+  }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <ReactFullpage
+          navigation={true}
+          navigationColor={["white"]}
+          scrollOverflow={true}
+          sectionsColor={["orange", "orange"]}
+          onLeave={this.onLeave.bind(this)}
+          afterLoad={this.afterLoad.bind(this)}
+          render={({ state, fullpageApi }) => {
+            return (
+              <>
+                <div id="fullpage-wrapper">
+                  {/* <Navbar /> */}
+                  <div className="section section1">
+                    <h3>Home</h3>
+                  </div>
+                  <div className="section">
+                    <h3>About Us</h3>
+                  </div>
+                  <div className="section">
+                    <h3>Services</h3>
+                  </div>
+                  <div className="section">
+                    <h3>Our Work</h3>
+                  </div>
+                  <div className="section">
+                    <h3>Track Order</h3>
+                  </div>
+                  <div className="section">
+                    <h3>Contact Us</h3>
+                  </div>
+                </div>
+              </>
+            );
+          }}
+        />
+      </>
+    );
+  }
+}
+
+ReactDOM.render(
+  <FullpageWrapper />,
+
+  document.getElementById("root")
+);
+
+export default FullpageWrapper;
