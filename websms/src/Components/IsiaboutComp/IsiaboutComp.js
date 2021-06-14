d9619574bda5c3e5c1a6113b2b47355d604d8ea0
NaufalRafi-hub <rafi.harahaps@gmail.com>
2021-06-15 01:15:00 +0700

    adding light animation on about us

--- websms/src/Components/IsiaboutComp/IsiaboutComp.js
+++ websms/src/Components/IsiaboutComp/IsiaboutComp.js
@@ -1,12 +1,21 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 // import url from "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;1,400&display=swap"
 
+export const test = keyframes `
+    100% {
+        opacity: 1;
+    }
+`
 export const Txtwrapper = styled.div`
-  margin-top: 90px;
+  margin-top: 100px;
   margin-right: auto;
   margin-left: auto;
   width: min(90%, 33em);
+  animation: ${test} 0.5s ease-out forwards;
+  opacity: 0;
 `;
+
+
 
 export const Headline = styled.h1`
   font-size: 50px;
@@ -15,6 +24,7 @@ export const Headline = styled.h1`
   font-weight: bold;
   text-align: center;
   align-items: center;
+  animation: ${test} 0.7s ease-out 3s forwards;
 `;
 
 export const Desc = styled.h2`
