651be74c06427b4c9235f338e3f76136649a95d6
NaufalRafi-hub <rafi.harahaps@gmail.com>
2021-07-20 16:05:47 +0700

    adding images & fixing data structure

--- websms/src/Components/Carousel/imageSlider.js
+++ websms/src/Components/Carousel/imageSlider.js
@@ -2,6 +2,11 @@ import First from '../../images/opt1.png';
 import logo1 from '../../images/heineken.png';
 import Second from '../../images/opt2.png';
 import logo2 from '../../images/cocacola.png';
+import bebelac from '../../images/bebelac.png';
+import camel from '../../images/camel.png';
+import dutch from '../../images/dutchmill.png';
+import mevius from '../../images/mevius.png';
+import sgm from '../../images/sgm.png';
 
 export default [
   {
@@ -14,70 +19,101 @@ export default [
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque. ',
         urls: Second,
         logo: logo1,
-        prod1 : 'Floor Display',
-        name1 : 'Bintang Radler Display',
-        desc1 : 
-        'Menggunakan material wood, acrylic, dan sticker digital printing. Process diawali dengan wood lathe, cutting acrylic, dan menginstall sticker digital printing',
-        prod2 : 'Table Display',
-        name2 : 'Fayrouz Display',
-        desc2 : 
-        'Menggunakan material acrylic, dan sticker digital printing. Process diawali dengan cutting acrylic, dan menginstall sticker digital printing',
-        prod3 : 'Bar Top', 
-        name3 : 'Heineken Display', 
-        desc3 : 'Menggunakan material acrylic, led light, sticker cutting. Process diawali dengan cutting acrylic, insttallation sticker cutting, installation LED light dan electricity', 
-        prod4 : 'Table Display', 
-        name4 : 'Radler Bottle Display', 
-        desc4 : 'Menggunakan material acrylic, dan sticker digital printing. Process diawali dengan cutting acrylic, dan installation sticker digital printing', 
+        product : [
+          {
+            prod : 'Floor Display',
+            name : 'Bintang Radler Display',
+            desc : 
+            'Menggunakan material wood, acrylic, dan sticker digital printing. Process diawali dengan wood lathe, cutting acrylic, dan menginstall sticker digital printing',
+          },
+          {
+            prod : 'Table Display',
+            name : 'Fayrouz Display',
+            desc : 
+            'Menggunakan material acrylic, dan sticker digital printing. Process diawali dengan cutting acrylic, dan menginstall sticker digital printing',
+          },
+          {
+            prod : 'Bar Top', 
+            name : 'Heineken Display', 
+            desc : 'Menggunakan material acrylic, led light, sticker cutting. Process diawali dengan cutting acrylic, insttallation sticker cutting, installation LED light dan electricity', 
+          },
+          {
+            prod : 'Table Display', 
+            name : 'Radler Bottle Display', 
+            desc : 'Menggunakan material acrylic, dan sticker digital printing. Process diawali dengan cutting acrylic, dan installation sticker digital printing', 
+          },
+        ]
       },
       {
         title: 'PT. Dutch Mill Indonesia*',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque.',
         urls: First,
-        logo: logo2,
-        prod1 : 'Floor Display', 
-        name1 : 'Dutchmill Drinks Display', 
-        desc1 : 'Menggunakan material wood, acrylic, dan sticker digital printing. Process diawali dengan wood lathe, cutting acrylic, dan installation sticker digital printing',
+        logo: dutch,
+        product : [
+          {
+            prod : 'Floor Display', 
+            name : 'Dutchmill Drinks Display', 
+            desc : 'Menggunakan material wood, acrylic, dan sticker digital printing. Process diawali dengan wood lathe, cutting acrylic, dan installation sticker digital printing',
+          },
+        ]
       },
       {
         title: 'PT. Karyadibya Mahardhika (JTI)*',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque.',
         urls: Second,
-        logo: logo2,
-        prod1 : 'Backwall Display', 
-        name1 : 'Camel Cigarettes Backwall Display', 
-        desc1 : 'Menggunakan material alumumium iron, acrylic, led light, sticker digital printing, digital printing untuk header. Process diawali alumunium welding, cutting acrylic, installation sticker digital printing, installation LED Light dan electricity',
+        logo: camel,
+        product : [
+          {
+            prod : 'Backwall Display', 
+            name : 'Camel Cigarettes Backwall Display', 
+            desc : 'Menggunakan material alumumium iron, acrylic, led light, sticker digital printing, digital printing untuk header. Process diawali alumunium welding, cutting acrylic, installation sticker digital printing, installation LED Light dan electricity',
+          },
+        ]
       },
       {
         title: 'PT. Mevius Indonesia*',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque.',
         urls: First,
-        logo: logo2,
-        prod1 : 'Bar Top', 
-        name1 : 'Mevius Cigarettes Display', 
-        desc1 : 'Menggunakan material acrylic, led light, dan sticker cutting. Process diawali dengan cutting acrylic, installation sticker cutting, installation LED light dan electricity',
+        logo: mevius,
+        product : [
+          {
+            prod : 'Bar Top', 
+            name : 'Mevius Cigarettes Display', 
+            desc : 'Menggunakan material acrylic, led light, dan sticker cutting. Process diawali dengan cutting acrylic, installation sticker cutting, installation LED light dan electricity',
+          },
+        ]
       },
       {
         title: 'PT. Nutricia Indonesia Sejahtera*',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque.',
         urls: Second,
-        logo: logo2,
-        prod1 : 'Shelftalker', 
-        name1 : 'Bebelac Go! Shelftalker Display', 
-        desc1 : 'Menggunakan material acrylic, dan Sticker digital printing. Process diawali dengan cutting acrylic, installation sticker digital printing',
+        logo: bebelac,
+        product : [
+          {
+            prod : 'Shelftalker', 
+            name : 'Bebelac Go! Shelftalker Display', 
+            desc : 'Menggunakan material acrylic, dan Sticker digital printing. Process diawali dengan cutting acrylic, installation sticker digital printing',
+          },
+        ]
       },
       {
         title: 'PT. Sarihusada Generasi Mahardhika*',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque.',
         urls: First,
-        logo: logo2,
-        prod1 : 'Shelftalker', 
-        name1 : 'SGM aktif Shelftalker Display', 
-        desc1 : 'Menggunakan material acrylic, dan Sticker digital printing. Process diawali dengan cutting acrylic, installation sticker digital printing',
+        logo: sgm,
+        product : [
+          {
+            prod : 'Shelftalker', 
+            name : 'SGM aktif Shelftalker Display', 
+            desc : 'Menggunakan material acrylic, dan Sticker digital printing. Process diawali dengan cutting acrylic, installation sticker digital printing',
+          },
+        ]
+        
       },
     ],
   },
@@ -108,31 +144,34 @@ export default [
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque. ',
         urls: First,
-        logo: logo2,
-        prod1 : 'DJ Table',
-        name1 : 'Heineken DJ Table',
-        desc1 : 'Menggunakan material wood, acrylic, led light, dan sticker cutting. Process diawali dengan cutting wood, cutting acrylic, installation sticker cutting, dan installation LED light dan electricity',
-        prod2 : 'Table Booth',
-        name2 : 'Bintang Radler Table Booth',
-        desc2 : 'Menggunakan material acrylic, LED Light, sticker cutting. Process diawali dengan cutting acrylic, installation sticker cutting, dan installation LED light dan electricity',
-
+        logo: logo1,
+        product : [
+          {
+            prod : 'DJ Table',
+            name : 'Heineken DJ Table',
+            desc : 'Menggunakan material wood, acrylic, led light, dan sticker cutting. Process diawali dengan cutting wood, cutting acrylic, installation sticker cutting, dan installation LED light dan electricity',
+          },
+          {
+            prod : 'Table Booth',
+            name : 'Bintang Radler Table Booth',
+            desc : 'Menggunakan material acrylic, LED Light, sticker cutting. Process diawali dengan cutting acrylic, installation sticker cutting, dan installation LED light dan electricity',
+          },
+        ]
       },
       {
         title: 'PT. Nutricia Indonesia Sejahtera*',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque.',
         urls: Second,
-        logo: logo1,
-        prod1 : 'Booth', 
-        name1 : 'Bebelac Booth',
-        desc1 : 'Menggunakan material wood, acrylic, dan sticker digital printing. Process diawali dengan cutting wood, cutting acrylic, painting, installation sticker digital printing',
-      },
-      {
-        title: 'Third Slider Image',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque.',
-        urls: First,
-        logo: logo1,
+        logo: bebelac,
+        product : [
+          {
+            prod : 'Booth', 
+            name : 'Bebelac Booth',
+            desc : 'Menggunakan material wood, acrylic, dan sticker digital printing. Process diawali dengan cutting wood, cutting acrylic, painting, installation sticker digital printing',
+          },
+        ]
+        
       },
     ],
   },
@@ -144,23 +183,33 @@ export default [
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque. ',
         urls: First,
-        logo: logo2,
-        prod1 : 'Totem',
-        name1 : 'Heineken Bottle Totem',
-        desc1 : 'Menggunakan material wood, acrylic, LED light, sticker cutting. Process diawali dengan wood lathe, cutting acrylic, installation sticker cutting, installation LED light dan electricity',
+        logo: logo1,
+        product : [
+          {
+            prod : 'Totem',
+            name : 'Heineken Bottle Totem',
+            desc : 'Menggunakan material wood, acrylic, LED light, sticker cutting. Process diawali dengan wood lathe, cutting acrylic, installation sticker cutting, installation LED light dan electricity',
+          },
+        ]
       },
       {
         title: 'PT. Karyadibya Mahardhika (JTI)*',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra pretium vel mattis posuere nulla venenatis rhoncus nec scelerisque.',
         urls: Second,
-        logo: logo1,
-        prod1 : 'Standee Acrylic',
-        name1 : 'Camel Standee Acrylic',
-        desc1 : 'Menggunakan material acrylic, dan sticker digital printing. Process diawali dengan cutting acrylic, dan installation sticker digital printing',
-        prod2 : 'Tin Can', 
-        name2 : 'Camel Tin Can Packaging',
-        desc2 : 'Mengguankan material cans, dan sticker digital printing. Process diawali dengan cleaning cans, installation sticker digital printing',
+        logo: camel,
+        product : [
+          {
+            prod : 'Standee Acrylic',
+            name : 'Camel Standee Acrylic',
+            desc : 'Menggunakan material acrylic, dan sticker digital printing. Process diawali dengan cutting acrylic, dan installation sticker digital printing',
+          },
+          {
+            prod : 'Tin Can', 
+            name : 'Camel Tin Can Packaging',
+            desc : 'Mengguankan material cans, dan sticker digital printing. Process diawali dengan cleaning cans, installation sticker digital printing',
+          },
+        ]
       },
     ],
   },
