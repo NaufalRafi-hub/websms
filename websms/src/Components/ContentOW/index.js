5e608a14315847f316a8b298d162be17458b037c
NaufalRafi-hub <rafi.harahaps@gmail.com>
2021-07-21 17:11:54 +0700

    bug fixes for link router

--- websms/src/Components/ContentOW/index.js
+++ websms/src/Components/ContentOW/index.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import imageSlider from "../Carousel/imageSlider";
 
 const ContentOW = () => {
-    return (
-        <div className="container">
-            
+  console.log(imageSlider);
+  return (
+    <>
+        {/* {imageSlider[0].data[0].product.map((prod, index) => (
+            <h1>{prod.name}</h1>
+        ))} */}
+        {/* test */}
+      {imageSlider[0].data[0].product.map((data, index) => (
+        <div key={index}>
+          <h1>{data.title}</h1>
+          <h2>{data.description}</h2>
         </div>
-    )
-}
+      ))}
+    </>
+  );
+};
 
-export default ContentOW
+export default ContentOW;
