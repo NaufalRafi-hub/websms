047dddebe811533d913e6567d9f885014eff131d
NaufalRafi-hub <rafi.harahaps@gmail.com>
2021-07-23 03:45:55 +0700

    fix styling on back icon

--- websms/src/Components/HeadlineAbtComp/AboutdetailComp.js
+++ websms/src/Components/HeadlineAbtComp/AboutdetailComp.js
@@ -1,25 +1,21 @@
-import styled from 'styled-components'
-import { MdKeyboardBackspace } from 'react-icons/md';
+import styled from "styled-components";
+import { MdKeyboardBackspace } from "react-icons/md";
 
 export const BackIcon = styled(MdKeyboardBackspace)`
   color: #24275c;
-  // width: 500;
-  &:after {
-   width: 40px;
-  }
-  `
+  font-size: 3rem;
+  // &:hover {
+  //   color: #FFCC33;
+  // }
+`;
 
-export const BackIconDiv = styled.div `
+export const BackIconDiv = styled.div`
   position: absolute;
   top: 20px;
   background: transparent;
   border: transparent;
-  font-size: 2.2rem;
+  // font-size: 2.2rem;
   cursor: pointer;
   outline: none;
   display: flex;
-  text-decoration: none;
-  &:after {
-    font-size: 30px;
-  }
-`
+`;
