14adca49de2d339e5e0df594a9822f83b26d1b99
NaufalRafi-hub <rafi.harahaps@gmail.com>
2021-07-30 16:58:19 +0700

    fix hero section about us responsive layout

--- websms/src/Components/HeroAboutComp/HeroAboutElements.js
+++ websms/src/Components/HeroAboutComp/HeroAboutElements.js
@@ -2,14 +2,17 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 export const Headline = styled(motion.h1)`
-  font-size: 70px;
+  font-size: 4.5rem;
   font-family: "Viga", sans-serif;
   color: #ffcc33;
   font-weight: bold;
+  @media screen and (max-width:998px) {
+    // font-size: 3.5rem;
+  }
 `;
 
 export const Subhead = styled(motion.h2)`
-  font-size: 23px;
+  font-size: 1.3rem;
   font-family: "Open Sans", sans-serif;
   text-align: justify;
   text-justify: auto;
